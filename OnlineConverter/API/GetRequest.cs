b32fe4cd0b6a5e35010935e43206c30b9c8ee4c2
MorpexX <morpexx08@gmail.com>
2023-11-26 19:42:52 +0200

    Створення функціоналу для Get методу API

--- OnlineConverter/API/GetRequest.cs
+++ OnlineConverter/API/GetRequest.cs
@@ -1,6 +1,38 @@
-﻿namespace OnlineConverter.API
+﻿using System.Net;
+
+namespace OnlineConverter.API
 {
     public class GetRequest
     {
+        HttpWebRequest _request;
+
+        private string _address;
+
+        public string Response {  get; set; }   
+
+        public GetRequest(string addres)
+        {
+            _address = addres; 
+        }
+
+        public void Run()
+        {
+            _request = (HttpWebRequest)WebRequest.Create(_address);
+            _request.Method = "GET";
+
+            try
+            {
+                HttpWebResponse response = (HttpWebResponse)_request.GetResponse();
+                var stream = response.GetResponseStream();
+
+                if (stream != null)
+                {
+                    Response = new StreamReader(stream).ReadToEnd();
+                }
+            }
+            catch (Exception)
+            {               
+            }
+        }
     }
 }
