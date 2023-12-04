2949463318a9494d34ffdad76d6f4dc0064344c1
MorpexX <morpexx08@gmail.com>
2023-12-04 22:19:07 +0200

    Додавання моделі валют Currency

---
+++ OnlineConverter/Models/Currency.cs
@@ -0,0 +1,10 @@
+﻿namespace OnlineConverter.Models
+{
+    public class Currency
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public double Price { get; set; }
+    }
+}
