0b3393aba203e114698642850cc8e48cd5c89af4
MorpexX <morpexx08@gmail.com>
2023-12-08 19:01:10 +0200

    Додали атрибути до полів

--- OnlineConverter/Models/CurrencyJson.cs
+++ OnlineConverter/Models/CurrencyJson.cs
@@ -1,9 +1,16 @@
-﻿namespace OnlineConverter.Models
+﻿using Newtonsoft.Json;
+
+namespace OnlineConverter.Models
 {
     public class CurrencyJson
     {
+        [JsonProperty("txt")]
         public string Name {  get; init; }
+
+        [JsonProperty("cc")]
         public string Code { get; init; }
+
+        [JsonProperty("rate")]
         public double Price { get; init; }
     }
 }
