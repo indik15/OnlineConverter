f729cd6d53e2bbd73ff68d87becd85356fad3d5f
MorpexX <morpexx08@gmail.com>
2023-12-08 18:23:13 +0200

    Модель для Json

---
+++ OnlineConverter/Models/CurrencyJson.cs
@@ -0,0 +1,9 @@
+﻿namespace OnlineConverter.Models
+{
+    public class CurrencyJson
+    {
+        public string Name {  get; init; }
+        public string Code { get; init; }
+        public double Price { get; init; }
+    }
+}
