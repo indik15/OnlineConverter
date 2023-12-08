14d1e15a5f89fa16b7697e58c3c51828b6f65d3b
MorpexX <morpexx08@gmail.com>
2023-12-08 18:16:02 +0200

    Створили ViewModel для валют

---
+++ OnlineConverter/Models/ViewModel/CurrencyVM.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace OnlineConverter.Models.ViewModel
+{
+    public class CurrencyVM
+    {
+        public Currency currency { get; set; }
+
+        public IEnumerable<SelectListItem> selectCurrency { get; set; }
+    }
+}
