718f42800880501bd27cca77ad2a9162fbfa66fd
MorpexX <morpexx08@gmail.com>
2023-12-09 22:29:48 +0200

    Добавили додаткові поля

--- OnlineConverter/Models/ViewModel/CurrencyVM.cs
+++ OnlineConverter/Models/ViewModel/CurrencyVM.cs
@@ -4,8 +4,10 @@ namespace OnlineConverter.Models.ViewModel
 {
     public class CurrencyVM
     {
+        public int inputNum { get; set; }
+        public string firstNum {  get; set; }
+        public string secondNum { get; set; }
         public Currency Currency { get; set; }
-
         public IEnumerable<SelectListItem> CurrencySelectList { get; set; }
     }
 }
