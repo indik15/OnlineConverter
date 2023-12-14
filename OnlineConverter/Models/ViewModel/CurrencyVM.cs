48b3fe59c2c8ae4cd458738e15c2a7d5679a1e93
MorpexX <morpexx08@gmail.com>
2023-12-14 23:39:11 +0200

    Спростили клас

--- OnlineConverter/Models/ViewModel/CurrencyVM.cs
+++ OnlineConverter/Models/ViewModel/CurrencyVM.cs
@@ -4,9 +4,7 @@ namespace OnlineConverter.Models.ViewModel
 {
     public class CurrencyVM
     {
-        public int inputNum { get; set; }
-        public string firstNum {  get; set; }
-        public string secondNum { get; set; }
+        public double convertedNumber { get; set; }
         public Currency Currency { get; set; }
         public IEnumerable<SelectListItem> CurrencySelectList { get; set; }
     }
