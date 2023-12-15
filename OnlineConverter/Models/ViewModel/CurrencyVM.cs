ee2d0c9266af734e0384707ab73a91d319f811a2
MorpexX <morpexx08@gmail.com>
2023-12-15 17:10:29 +0200

    Додали поле типу bool для перевірки можливості конвертації

--- OnlineConverter/Models/ViewModel/CurrencyVM.cs
+++ OnlineConverter/Models/ViewModel/CurrencyVM.cs
@@ -4,6 +4,7 @@ namespace OnlineConverter.Models.ViewModel
 {
     public class CurrencyVM
     {
+        public bool isCanConverted { get; set; } = true;
         public double convertedNumber { get; set; }
         public Currency Currency { get; set; }
         public IEnumerable<SelectListItem> CurrencySelectList { get; set; }
