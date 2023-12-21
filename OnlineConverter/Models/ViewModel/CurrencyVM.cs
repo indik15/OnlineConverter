2a5e3cc535269da88d478017e35e8d2d775078e8
MorpexX <morpexx08@gmail.com>
2023-12-21 22:06:50 +0200

    Додали поля для блоку "Актуальний курс"

--- OnlineConverter/Models/ViewModel/CurrencyVM.cs
+++ OnlineConverter/Models/ViewModel/CurrencyVM.cs
@@ -4,6 +4,10 @@ namespace OnlineConverter.Models.ViewModel
 {
     public class CurrencyVM
     {
+        public bool EurIsUpdated { get; set; }
+        public bool UsdIsUpdated { get; set; }
+        public double Usd { get; set; }
+        public double Eur { get; set; }
         public double ConvertedNumber { get; set; }
         public Currency Currency { get; set; }
         public IEnumerable<SelectListItem> CurrencySelectList { get; set; }
