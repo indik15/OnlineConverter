cb9fba84bf8a9387058546acc1f84118f218e9bb
MorpexX <morpexx08@gmail.com>
2023-12-28 17:54:53 +0200

    Створили міграції для блоку актуального курсу

---
+++ OnlineConverter/Migrations/20231228143911_addCurrentRateToDatabase.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OnlineConverter.Migrations
+{
+    /// <inheritdoc />
+    public partial class addCurrentRateToDatabase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CurrentRates",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EurIsUpdated = table.Column<bool>(type: "bit", nullable: false),
+                    UsdIsUpdated = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CurrentRates", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CurrentRates");
+        }
+    }
+}
