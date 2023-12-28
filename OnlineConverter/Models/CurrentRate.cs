7e1b1b6ed150f82989d88e6cc0d24573edcd46c8
MorpexX <morpexx08@gmail.com>
2023-12-28 17:56:42 +0200

    Створили модель для блоку актуального курсу

---
+++ OnlineConverter/Models/CurrentRate.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineConverter.Models
+{
+    public class CurrentRate
+    {
+        [Key]
+        public int Id { get; set; }
+        public bool EurIsUpdated { get; set; }
+        public bool UsdIsUpdated { get; set; }
+    }
+}
