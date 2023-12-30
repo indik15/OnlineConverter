fec8827d4ff0b117f0a44181505e9f3de4a6a909
MorpexX <morpexx08@gmail.com>
2023-12-30 19:14:43 +0200

    Створили модель для євро для графіка

---
+++ OnlineConverter/Models/EurGraph.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineConverter.Models
+{
+    public class EurGraph
+    {
+        [Key]
+        public int Id { get; set; }
+        public double Price { get; set; }
+        public string Date { get; set; }
+    }
+}
