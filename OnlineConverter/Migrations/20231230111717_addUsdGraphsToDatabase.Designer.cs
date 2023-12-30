b63b7f05cf1a46e2812f0ba37ffafcd44a10ec1c
MorpexX <morpexx08@gmail.com>
2023-12-30 19:07:19 +0200

    Додали міграції для об'єкта долара  для графіка

--- OnlineConverter/Migrations/20231230111717_addUsdGraphsToDatabase.Designer.cs
+++ OnlineConverter/Migrations/20231230111717_addUsdGraphsToDatabase.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using OnlineConverter.Data;
 
@@ -10,9 +11,11 @@ using OnlineConverter.Data;
 namespace OnlineConverter.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231230111717_addUsdGraphsToDatabase")]
+    partial class addUsdGraphsToDatabase
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
