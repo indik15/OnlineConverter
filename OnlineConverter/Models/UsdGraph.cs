85f9eadab1d81edf0a8f8f2a15914931fb801b61
MorpexX <morpexx08@gmail.com>
2023-12-30 19:05:01 +0200

    Створили модель долара для графіка

---
+++ OnlineConverter/Models/UsdGraph.cs
@@ -0,0 +1,10 @@
+﻿namespace OnlineConverter.Models
+{
+    public class UsdGraph
+    {
+        public int Id { get; set; }
+        public double Price { get; set; }
+        public string Date { get; set; }
+
+    }
+}
