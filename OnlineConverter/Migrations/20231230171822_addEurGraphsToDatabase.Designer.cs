77ef00eaceac2760056145d3d9010a00a9261a67
MorpexX <morpexx08@gmail.com>
2023-12-30 19:19:31 +0200

    Ствоили міграцію для євро для графіка

--- OnlineConverter/Migrations/20231230171822_addEurGraphsToDatabase.Designer.cs
+++ OnlineConverter/Migrations/20231230171822_addEurGraphsToDatabase.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using OnlineConverter.Data;
 
@@ -10,9 +11,11 @@ using OnlineConverter.Data;
 namespace OnlineConverter.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231230171822_addEurGraphsToDatabase")]
+    partial class addEurGraphsToDatabase
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ OnlineConverter/Migrations/20231230171822_addEurGraphsToDatabase.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OnlineConverter.Migrations
+{
+    /// <inheritdoc />
+    public partial class addEurGraphsToDatabase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "EurGraphs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Price = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EurGraphs", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EurGraphs");
+        }
+    }
+}
