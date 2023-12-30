cefdb42d13a0776412b476d1053a3683a1aec9a8
MorpexX <morpexx08@gmail.com>
2023-12-30 13:09:59 +0200

    Додали поля для графіка до VM

--- OnlineConverter/Models/ViewModel/CurrencyVM.cs
+++ OnlineConverter/Models/ViewModel/CurrencyVM.cs
@@ -4,6 +4,9 @@ namespace OnlineConverter.Models.ViewModel
 {
     public class CurrencyVM
     {
+        public List<string> Date { get; set; }
+        public List<double> GraphListUSD { get; set; }
+        public List<double> GraphListEUR { get; set; }
         public double Usd { get; set; }
         public double Eur { get; set; }
         public double ConvertedNumber { get; set; }
