94be37269ace0c53950cd5d5c30f9d8093e5d215
MorpexX <morpexx08@gmail.com>
2023-12-30 19:14:15 +0200

    Додали атрибут Key

--- OnlineConverter/Models/UsdGraph.cs
+++ OnlineConverter/Models/UsdGraph.cs
@@ -1,7 +1,10 @@
-﻿namespace OnlineConverter.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineConverter.Models
 {
     public class UsdGraph
     {
+        [Key]
         public int Id { get; set; }
         public double Price { get; set; }
         public string Date { get; set; }
